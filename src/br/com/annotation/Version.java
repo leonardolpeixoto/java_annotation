df4b2e8a28cf2fcb79fa4da5470241ae76b1a596
leonardo <leonardodelimapeixoto@gmail.com>
2018-03-04 20:52:12 -0300

    Annotation Test

---
+++ src/br/com/annotation/Version.java
@@ -0,0 +1,6 @@
+package br.com.annotation;
+
+public @interface Version {
+    int major();
+    int minor();
+}
---
+++ src/br/com/annotation/VersionTest.java
@@ -0,0 +1,7 @@
+// pag 12
+package br.com.annotation;
+
+@Version(major = 1, minor = 0)
+public class VersionTest{
+    public int xyz = 110;   
+}
